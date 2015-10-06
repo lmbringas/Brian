f727688afaee5f6b5c7c2e7cede10884b90a8a46
lmbringas <lm.bringas98@gmail.com>
2015-10-06 13:40:40 -0300

    borra buu

--- buu.sh
+++
@@ -1,4 +0,0 @@
-wget -q -U "Mozilla/5.0" --post-file output.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com.mx/speech-api/v2/recognize?output=json&lang=es&key=AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw"   | cut -d\" -f8  > stt.txt
- 
-value=`cat stt.txt`
-echo "$value"
